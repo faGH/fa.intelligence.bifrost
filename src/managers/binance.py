03543c4b6f97c152c1ced99c0aba21807992c0f0
Dean Martin <dean.martin@frostaura.net>
2021-11-28 07:50:33 +0200

    [DEV]: Completion of the Binance pair forecasting signature.

--- src/managers/binance.py
+++ src/managers/binance.py
@@ -12,18 +12,18 @@ forecast_response_model = get_forecast_response(api)
 class SpotPairForecast(Resource):
     @api.doc('Spot Pair Forecast', params={
         'pair_name': {'description': 'The pair name that matches that of the Binance exchange for which to forecast. This value is case-insensitive and the underscore is optional.'},
-        'cutoff_time': {'in': 'query', 'description': 'The max cutoff time for market data to use. This allows for excluding future data when performing tasks like back-testing.', 'default': f'{now}'},
+        'cutoff_time_utc': {'in': 'query', 'description': 'The max cutoff time for market data to use. This allows for excluding future data when performing tasks like back-testing.', 'default': f'{now}'},
         'n_forecasts': {'in': 'query', 'description': 'The number of forecasts to make. These forecasts will be the same window size as the period.', 'default': '1'}
     })
-    @api.marshal_list_with(forecast_response_model)
+    @api.marshal_with(forecast_response_model)
     def get(self, pair_name):
         '''Gets X forecasts on a specific pair for the Binance exchange, given a specific cut-off time.'''
         parser = reqparse.RequestParser()
         parser.add_argument('n_forecasts', type=int, default=1)
-        parser.add_argument('cutoff_time', type=str, default=now)
+        parser.add_argument('cutoff_time_utc', type=str, default=now)
         args = parser.parse_args()
         
-        cutoff_time = args['cutoff_time']
+        cutoff_time_utc = args['cutoff_time_utc']
         n_forecasts = args['n_forecasts']
         
-        return { 'response': f'Forecast for pair "{pair_name}" requested. Cutoff: {cutoff_time}. Forecasts: {n_forecasts}.' }, 200
+        return { 'response': f'Forecast for pair "{pair_name}" requested. Cutoff: {cutoff_time_utc}. Forecasts: {n_forecasts}.' }, 200
--- src/models/binance.py
+++ src/models/binance.py
@@ -2,8 +2,13 @@
 from flask_restx import fields
 
 def get_forecast_response(api):
+    forecast_model = api.model('Forecast', {
+        'timestamp_utc': fields.DateTime(description='The time for which this forecast is applicable.'),
+        'close': fields.Float(description='The forecasted closing price of the asset.')
+    })
     response_model = api.model('SpotPairForecastResponse', {
-        'response': fields.String(description="Forecast response")
+        'pairName': fields.String(description='The pair name used on Binance to fetch market data.'),
+        'forecasts': fields.List(fields.Nested(forecast_model))
     })
     
     return response_model
