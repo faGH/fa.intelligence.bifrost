cf83affbaccc5665523664b393ed7ab056ce4b0e
Dean Martin <dean.martin@derivco.co.za>
2022-06-23 14:00:45 +0200

    [DEV]: First legit test added.

--- test/t1/test_pytest_example.py
+++
@@ -1,7 +0,0 @@
-'''This module hosts example pytest test cases that can be referred to as a template.'''
-
-def test_example_method_with_no_params_passed_should_pass():
-    '''Naming structure "<function_name>_<given_conditions>_<expected_result>"'''
-
-    # More examples: https://realpython.com/pytest-python-testing/
-    assert True
